ad32db40d321372ca3499e315ae0c33ef1e5dd28
holly <hollage0214@gmail.com>
2022-06-01 23:19:30 +0900

    javascriptが動作するように修正

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
+var convMap = new Map([
+  ["ども！", "おせわになっております。"],
+  ["マジで", "大変"],
+  ["ごめんなさい", "申し訳ありません。"],
+  ["ごめん", "申し訳ありません。"],
+  ["before", "after"],
+  ["before", "after"],
+  ["before", "after"],
+  ["before", "after"]
+]);
+
 function genTxt() {
-  alert("ボタンがクリックされました");
+  var txt = document.getElementById("befGen").value;
+  // alert(txt);
+
+  txt.split("").map(function(num){
+    return convMap.hasOwnProperty(num) ? convMap[num] : num;
+}).join("");
+
+  document.getElementById("aftGen").value = txt;
 }
